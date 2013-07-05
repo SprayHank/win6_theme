e5248b6997f11e77e3883fae32d7e3c6645dbbd4
SprayHank <fantasyoui@gmail.com>
2013-07-05 18:15:51 +0800

    修改

--- js/searchform.js
+++ js/searchform.js
@@ -8,6 +8,98 @@
 (function(w, d){
 
 seajs.use(['jquery/jqueryui/fg.menu'], function(jQuery) {
+	/**/var $ = jQuery;
+	var ruls = '';
+	var mainColor = 'cd8101';
+	var mainWidth = 160;
+	var lightColor = 'F4FFBA';
+	var sideWidth = 3;
+	var radioPercent = 80;
+	ruls += '.wrapper-dropdown-1 { /* Size and position */position: relative; /* Enable absolute positionning for children and pseudo elements */width: '+mainWidth+'px; padding: '+sideWidth+'px '+Math.ceil(sideWidth*1.8)+'px; margin: 0 auto; /* Styles */ background: #'+mainColor+'; color: #'+lightColor+'; outline: none; cursor: pointer; /* Font settings */ font-weight: bold; }'
+		+'.wrapper-dropdown-1:after { content: ""; width: 0; height: 0; position: absolute; right: '+Math.ceil(sideWidth*2*1.8)+'px; top: 50%; margin-top: -6px; border-width: 6px 0 6px 6px; border-style: solid; border-color: transparent #'+lightColor+'; }'
+		+'.dropdown-positionHelper { display: none; z-index:9999999999 }'
+		+'.dropdown-positionHelper .dropdown { /* Size & position */ position: absolute; top: 100%; left: 0; right: 0; /* Styles */ background: #'+lightColor+'; list-style: none; font-weight: normal; /* Cancels previous font-weight: bold; *//* Hiding */ display: none; pointer-events: none; }'
+		+'.dropdown-positionHelper .dropdown li a { display: block; text-decoration: none; color: #9E9E9E; padding: '+sideWidth+'px '+(sideWidth*2*1.8)+'px; }'
+		+'.dropdown-positionHelper .dropdown li:hover a {/* Hover state */ background: #F3F8F8; }'
+		+'.dropdown-positionHelper.active .dropdown {/* Active state */ display: block; pointer-events: auto; }'
+		+'.dropdown-positionHelper.active { display: block; overflow: visible; }'
+		+'.wrapper-dropdown-1.active:after { border-color: #'+mainColor+' transparent; border-width: 6px 6px 0 6px; margin-top: -3px; }'
+		+'.wrapper-dropdown-1.active { background: #'+mainColor+'; background: -webkit-gradient(linear, left top, right top, color-stop(0%, #'+mainColor+'), color-stop('+radioPercent+'%, #'+mainColor+'), color-stop('+radioPercent+'%, #'+lightColor+'), color-stop(100%, #'+lightColor+')); background: -webkit-linear-gradient(left, #'+mainColor+' 0%, #'+mainColor+' '+radioPercent+'%, #'+lightColor+' '+radioPercent+'%, #'+lightColor+' 100%); background: -moz-linear-gradient(left, #'+mainColor+' 0%, #'+mainColor+' '+radioPercent+'%, #'+lightColor+' '+radioPercent+'%, #'+lightColor+' 100%); background: -ms-linear-gradient(left, #'+mainColor+' 0%, #'+mainColor+' '+radioPercent+'%, #'+lightColor+' '+radioPercent+'%, #'+lightColor+' 100%); background: -o-linear-gradient(left, #'+mainColor+' 0%, #'+mainColor+' '+radioPercent+'%, #'+lightColor+' '+radioPercent+'%, #'+lightColor+' 100%); background: linear-gradient(to right, #'+mainColor+' 0%, #'+mainColor+' '+radioPercent+'%, #'+lightColor+' '+radioPercent+'%, #'+lightColor+' 100%); filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#'+mainColor+'", endColorstr="#'+lightColor+'", GradientType=1); }';
+	var style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML=ruls;
+	document.getElementsByTagName('HEAD').item(0).appendChild(style);
+	$('#search').after('<div id="dropdown1" class="wrapper-dropdown-1" tabindex="1" style="float:left;">'+
+		'<span>类别：所有分类</span>'+
+		'<div style="display:none;">'+
+		'<ul id="droplist1" class="dropdown" tabindex="1">'+
+		'</ul>'+
+		'</div>'+
+		'</div>');
+	var op = d.getElementById('category').getElementsByTagName('option');
+	var itop;
+	for(var i=0;itop = op[i];i++){
+		$('#droplist1').append('<li value="'+itop.value+'"><a href="#">'+itop.innerHTML+'</a></li>');
+	}
+	document.getElementById('category').parentNode.replaceChild(document.getElementById('dropdown1'), document.getElementById('category'));
+	function DropDown(el) {
+		this.dropdown = el;
+		this.placeholder = this.dropdown.children('span');
+		this.val = '';
+		this.index = -1;
+		var obj = this;
+		obj.wrapDropDown();
+		this.opts = this.droplist.find('#droplist1 > li');
+		obj.dropdown.on('click', function(event) {
+			$(this).toggleClass('active');
+			$(obj.droplist).toggleClass('active');
+			return false;
+		});
+		obj.opts.on('click', function() {
+			var opt = $(this);
+			obj.val = opt.text();
+			obj.value = opt.attr('value');
+			obj.placeholder.text('类别: ' + obj.val);
+			$('#category').attr('value', obj.value);
+		});
+		$(document).click(function() {
+			// all dropdowns
+			$(obj.dropdown).removeClass('active');
+			$(obj.droplist).removeClass('active');
+		});
+	};
+	DropDown.prototype = {
+		getValue: function() {
+			return this.value;
+		},
+		getIndex: function() {
+			return this.index;
+		}
+	};
+	DropDown.prototype.wrapDropDown = function(){
+		//var el = widget;
+		var referrer = $(this.dropdown);
+		var dims = {
+			refX: referrer.offset().left,
+			refY: referrer.offset().top,
+			refW: referrer.getTotalWidth(),
+			refH: referrer.getTotalHeight()
+		};
+		var helper = $('<div class="dropdown-positionHelper"></div>');
+		helper.css({ position: 'absolute', left: dims.refX, top: dims.refY, width: dims.refW, height: dims.refH }).appendTo('body');
+		document.body.childNodes[document.body.childNodes.length-1].appendChild(document.getElementById('droplist1'));
+		this.droplist = $(document.getElementById('droplist1').parentNode);
+	}
+
+	$(function() {
+		var dd = new DropDown($('#dropdown1'));
+	});
+
+	$('#keyword').css({margin: '0px 10px', padding: 0, float: 'left', height: 22});
+	$('#searchForm').css({height:30}).append('<input type="hidden" name="category" id="category" value="0" />');
+	$('.go').attr('value', '搜索').removeClass('go').css({border:'1px solid #333333'});
+
+
 //	var $ = jQuery;
 //	var locStr1 = w.libPath+'./jquery/jqueryui/fg.menu.css';
 //	var cssLink1 = jQuery('<link href="'+locStr1+'" type="text/css" rel="Stylesheet" class="ui-theme" />');
@@ -26,7 +118,7 @@ seajs.use(['jquery/jqueryui/fg.menu'], function(jQuery) {
 //	$('#category').remove();
 //	$('#search').after('<div id="replaceSearch" style="height:31px;margin-top:18px;float:right;"></div> ');
 //	$('#searchForm').appendTo('#replaceSearch').css({width:'490px',top:0})
-//		.append('<a tabindex="0" href="#search-engines" class="fg-button fg-button-icon-right ui-widget ui-state-default ui-corner-all" style="float:right;" id="flat"><span class="ui-icon ui-icon-triangle-1-s"></span><span id="category_text">'+itext+'</span></a>')
+//		.append('<a tabindex="0" href="#search-engines" class="fg-button fg-button-icon-right ui-widget ui-state-default" style="float:right;" id="flat"><span class="ui-icon ui-icon-triangle-1-s"></span><span id="category_text">'+itext+'</span></a>')
 //		.append('<input type="hidden" name="category" id="category" value="'+ivalue+'" />');
 //	$('#flat')
 //		.after('<span id="menuSelection"></span> ')
@@ -46,9 +138,9 @@ seajs.use(['jquery/jqueryui/fg.menu'], function(jQuery) {
 //	});
 //
 //	$('#keyword').addClass('ui-widget').removeClass('B_input').css({'height':'24px',width:'130px',float:'right'})
-//		.next().addClass('ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only').removeClass('go')
+//		.next().addClass('ui-button ui-widget ui-state-default ui-button-text-only').removeClass('go')
 //		.attr('value','搜索').css({float:'right','height':'31px'}).prependTo('#searchForm').next()
-//		.next().addClass('ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only')
+//		.next().addClass('ui-button ui-widget ui-state-default ui-button-text-only')
 //		.css({padding:'0.4em 1em',display:'inline-block',float:'right'}).prependTo('#searchForm');
 });
 })(window,document);
